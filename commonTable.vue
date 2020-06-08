dcb27278db30b477772f9174bc5fec6090b54701
第 <40297687+fight1991@users.noreply.github.com>
2020-06-08 19:12:22 +0800

    设置表格 高度和最大高度, 固定列宽和最小列宽

--- commonTable.vue
+++ commonTable.vue
@@ -1,93 +1,52 @@
+
 <template>
+<!-- eslint-disable vue/valid-v-bind -->
   <div style="position:relative">
-    <el-popover
-      popper-class="tableBtn-popper"
-      v-if="checked"
-    >
+  <!--  自定义表头显示项目数 -->
+    <el-popover popper-class="tableBtn-popper" v-if="checked">
       <ul>
-        <li
-          v-for="(value,key) in tableHeadData"
-          :key="'index' + key"
-        >
-          <el-checkbox
-            v-model="value.checked"
-            size="mini"
-          >{{value.label}}</el-checkbox>
+        <li v-for="(value,key) in tableHeadData" :key="'index' + key">
+          <el-checkbox v-model="value.checked" size="mini">{{value.label}}</el-checkbox>
         </li>
       </ul>
-      <el-button
-        class="poper-btn"
-        slot="reference"
-        icon="el-icon-s-grid"
-        plain
-        size="mini"
-      ></el-button>
+      <el-button class="poper-btn" slot="reference" icon="el-icon-s-grid" plain size="mini"></el-button>
     </el-popover>
     <!--  公共table列表 -->
-    <!-- 选中行 有问题  -->
-    <!-- @row-click="selectRow" -->
-    <el-table
-      ref="commonTable"
+    <el-table ref="commonTable"
       @select="selectSingleBox"
       @select-all="selectAllBox"
+      @row-click="selectRow"
       :data="tableList"
-      :max-height="tableHeight || tableH"
+      :[tableHeight.prop]="tableHeight.value"
       size="mini"
       highlight-current-row
-      :border="border"
-      :show-summary="showSummary"
-      :summary-method="summaryMethod"
-    >
-      <!-- 自定义表格列 -->
-      <el-table-column
-        v-if="$attrs.selfCol"
-        width="100"
-        :label="$attrs.selfCol && $attrs.selfCol.label || ''"
-        :prop="$attrs.selfCol && $attrs.selfCol.prop || ''"
-        align="center"
-      >
-      </el-table-column>
-      <el-table-column
-        v-if="selectBox"
-        type="selection"
-        width="55"
-      ></el-table-column>
-      <el-table-column
-        v-if="showNum"
-        type="index"
-        width="50"
-        label="序号"
-        align="center"
-      ></el-table-column>
+      :border="border">
+      <el-table-column v-if="selectBox" type="selection" align="center" width="40"></el-table-column>
+      <el-table-column v-if="showNum" type="index" width="50" label="NO." align="center"></el-table-column>
       <template v-for="(item,index) in trueTableHead">
         <el-table-column
+          show-overflow-tooltip
           v-if="!item.slotName"
           :key="'table' + index"
           :fixed="item.fixed || false"
           :prop="item.prop || ''"
-          :label="item.label"
-          :align="item.align || 'left'"
-          :min-width="item.minWidth || '120'"
-          :width="item.width || ''"
-        >
-        </el-table-column>
-        <el-table-column
-          v-else
+          :label="$t(item.label)"
+          :align="item.align || 'center'"
+          :[widthMethod(item.width)]="item.width || '80'"
+          :render-header="item.renderHeader ? renderHead : renderCommon"
+        ></el-table-column>
+        <el-table-column v-else
+          show-overflow-tooltip
           :key="'table' + index"
           :fixed="item.fixed || false"
           :prop="item.prop || ''"
-          :label="item.label"
-          :align="item.align || 'left'"
-          :min-width="item.minWidth || '120'"
-          :width="item.width || ''"
-        >
+          :label="$t(item.label)"
+          :align="item.align || 'center'"
+          :min-width="item.width || '80'"
+          :render-header="item.renderHeader ? renderHead : renderCommon">
           <template slot-scope="scope">
             <!-- 具名插槽 -->
-            <slot
-              :name="item.slotName || 'default'"
-              :row="scope.row"
-              :$index="scope.$index"
-            ></slot>
+            <slot :name="item.slotName || 'default'" :row="scope.row"></slot>
           </template>
         </el-table-column>
       </template>
@@ -95,33 +54,36 @@
   </div>
 </template>
 <script>
-/*
+/**
  *  @param :
  *  1. clearSelection --- 如果传过来的值是true时,清空所有选项,然后会自动改变父组件的值为false
  *  2. tableList -- 表格数据列表
  *  3. tableHeadData -- 表格表头数据
  *  4. selectBox --- 是否显示勾选框
  *  5. border --- 是否显示边框
- *  6. showSummary -- 是否显示汇总
  *  7. showNum -- 是否显示序号
- *  8. propertySum -- 求和项
  *  9. selectSingle -- 表格是否为单选
- */
+ *  10. tableHeight -- 表格高度 如果传入Height 定高, 否则自动设置成最大高
+ *  11. widthMethod -- 如果设置了item.width 列宽 则定宽, 否则自动设置成最小宽度
+*/
 export default {
   name: 'common-table',
-  data() {
+  data () {
     return {
-      tableH: window.innerHeight - this.$store.state.HEIGHT_DIFFERENCE,
-      selection: []
+      selection: [],
+      widthMethod (prop, value) {
+        return prop ? 'width' : 'min-width'
+      }
     }
   },
-  created() {},
   props: {
-    tableHeight: {
-      type: Number,
-      default: () => {
-        return 0
+    maxHeight: {
+      default () {
+        return window.innerHeight - this.$store.state.tableH
       }
+    },
+    height: {
+      default: ''
     },
     tableList: {
       type: Array,
@@ -132,16 +94,10 @@ export default {
     border: {
       type: Boolean,
       default: () => {
-        return true
+        return false
       }
     },
     checked: {
-      type: Boolean,
-      default: () => {
-        return true
-      }
-    },
-    showSummary: {
       type: Boolean,
       default: () => {
         return false
@@ -156,7 +112,7 @@ export default {
     selectBox: {
       type: Boolean,
       default: () => {
-        return true
+        return false
       }
     },
     showNum: {
@@ -169,35 +125,41 @@ export default {
         return []
       }
     },
-    propertySum: {
-      type: Array,
-      default: () => {
-        return ['totalPrice']
-      }
-    },
     selectSingle: {
       type: Boolean,
       default: false
     }
   },
   computed: {
-    trueTableHead() {
+    trueTableHead () {
       return this.tableHeadData.filter(v => v.checked)
+    },
+    tableHeight () {
+      if (this.height) {
+        return {
+          prop: 'height',
+          value: this.height
+        }
+      }
+      return {
+        prop: 'max-height',
+        value: this.maxHeight
+      }
     }
   },
   watch: {
-    clearSelection: function(newData) {
+    clearSelection: function (newData) {
       if (typeof newData === 'boolean' && newData) {
         newData && this.$refs['commonTable'].clearSelection()
         this.$emit('update:clearSelection', false)
       }
     }
   },
-  mounted() {
+  mounted () {
     // this.list(this.pageParams);
   },
   methods: {
-    selectSingleBox(selection, row) {
+    selectSingleBox (selection, row) {
       if (this.selectSingle) {
         // 设置为单选
         this.selection = [row]
@@ -209,7 +171,7 @@ export default {
       this.$emit('select', selection, row)
     },
     // 勾选选择框
-    selectAllBox(selection) {
+    selectAllBox (selection) {
       if (this.selectSingle) {
         this.$refs['commonTable'].clearSelection()
         this.selection = []
@@ -219,7 +181,7 @@ export default {
       this.$emit('select', this.selection)
     },
     // 点击表格行
-    selectRow(row) {
+    selectRow (row) {
       let index = this.selection.indexOf(row)
       if (this.selectSingle) {
         if (index > -1) {
@@ -243,47 +205,26 @@ export default {
       }
       this.$emit('select', this.selection)
     },
-    // 汇总规则
-    summaryMethod(param) {
-      const { columns, data } = param
-      const sums = []
-      columns.forEach((column, index) => {
-        if (index === 0) {
-          sums[index] = '合计:'
-          return
-        }
-        if (this.propertySum.includes(column.property)) {
-          const values = data.map(item => Number(item[column.property]))
-          if (!values.every(value => isNaN(value))) {
-            sums[index] = values.reduce((prev, curr) => {
-              const value = Number(curr)
-              if (!isNaN(value)) {
-                return prev + curr
-              } else {
-                return prev
-              }
-            }, 0)
-          } else {
-            sums[index] = ''
-          }
-        } else {
-          sums[index] = ''
-        }
-      })
-      return sums
+    // 定义表头溢出省略号
+    renderHead (h, { column }) {
+      return (
+        <el-tooltip
+          class="text-cut"
+          effect="dark"
+          content={column.label}
+          placement="top"
+        >
+          <div>{column.label}</div>
+        </el-tooltip>
+      )
     },
-    //勾选
-    toggleSelection(rows) {
-      if (rows) {
-        rows.forEach(row => {
-          this.$refs.multipleTable.toggleRowSelection(row)
-        })
-      } else {
-        this.$refs.multipleTable.clearSelection()
-      }
+    // 普通显示
+    renderCommon (h, { column }) {
+      return column.label
     }
   }
 }
 </script>
 <style scoped lang="less">
+
 </style>
